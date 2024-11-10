a7e9e1596c75b350565156dd169ec58afb58e2d4
banseok1216 <bansuk1216@naver.com>
2024-11-11 00:16:27 +0900

    refactor: media 실제 환경 확인

--- domain/src/main/kotlin/org/chewing/v1/implementation/media/FileGenerator.kt
+++ domain/src/main/kotlin/org/chewing/v1/implementation/media/FileGenerator.kt
@@ -1,25 +1,24 @@
 package org.chewing.v1.implementation.media
 
 import org.chewing.v1.model.media.*
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 
 @Component
-class FileGenerator {
+class FileGenerator(
+    @Value("\${media.url}") private val mediaUrl: String,
+) {
     fun generateMedias(
         files: List<FileData>,
         userId: String,
-        category: FileCategory
-    ): List<Pair<FileData, Media>> {
-        return files.map { file ->
-            Pair(file, Media.upload(category, userId, file.name, file.contentType))
-        }
+        category: FileCategory,
+    ): List<Pair<FileData, Media>> = files.map { file ->
+        Pair(file, Media.upload(mediaUrl, category, userId, file.name, file.contentType))
     }
 
     fun generateMedia(
         file: FileData,
         userId: String,
-        category: FileCategory
-    ): Media {
-        return Media.upload(category, userId, file.name, file.contentType)
-    }
+        category: FileCategory,
+    ): Media = Media.upload(mediaUrl, category, userId, file.name, file.contentType)
 }
